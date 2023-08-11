511c3e3b0ca24aabfd7dfda659e0c2a065374e85
veerbhayani <virbhayani137@gmail.com>
2023-08-11 10:39:26 +0530

    data not getting solved

--- lib/my_practice/simple_crud_app_demo/simple_crud_app.dart
+++ lib/my_practice/simple_crud_app_demo/simple_crud_app.dart
@@ -1,299 +1,194 @@
 import 'package:flutter/material.dart';
 import 'package:stream_demo/my_practice/simple_crud_app_demo/simple_crud_stream.dart';
 
-class SimpleCrudApp extends StatelessWidget {
-  const SimpleCrudApp({super.key});
+class SimpleCrudeApp extends StatelessWidget {
+  const SimpleCrudeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text.rich(
-          TextSpan(
-            text: 'Simple',
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
-            ),
-            children: [
-              TextSpan(
-                text: ' Crude',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Column(
+          children: [
+            StreamBuilder(
+              stream: SimpleCrudStream.nameStreamController,
+              builder: (context, snapshot) => TextField(
+                controller: SimpleCrudStream.txtNameEditingController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Name',
+                  labelText: 'Name',
                 ),
               ),
-              TextSpan(
-                text: ' App',
-                style: TextStyle(
-                  color: Colors.yellow,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
+            ),
+            const SizedBox(height: 5),
+            StreamBuilder(
+              stream: SimpleCrudStream.middleNameStreamController,
+              builder: (context, snapshot) => TextField(
+                controller: SimpleCrudStream.txtMiddleNameEditingController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'MiddleName',
+                  labelText: 'MiddleName',
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-      body: ListView(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 15,
-              top: 5,
-              bottom: 5,
             ),
-            child: Text(
-              "User Details",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
+            const SizedBox(height: 5),
+            StreamBuilder(
+              stream: SimpleCrudStream.lastNameStreamController,
+              builder: (context, snapshot) => TextField(
+                controller: SimpleCrudStream.txtLastNameEditingController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'LastName',
+                  labelText: 'LastName',
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: SimpleCrudStream.txtNameEditingController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                      25,
-                    ),
+            const SizedBox(height: 5),
+            StreamBuilder(
+              stream: SimpleCrudStream.genderStreamController,
+              builder: (context, snapshot) => Row(
+                children: [
+                  const Text('Gender :  '),
+                  const Text('Male'),
+                  Radio(
+                    value: SimpleCrudStream.male,
+                    groupValue: SimpleCrudStream.gender,
+                    onChanged: (value) {
+                      SimpleCrudStream.genderControllerValue =
+                          SimpleCrudStream.gender = value!;
+                    },
                   ),
-                ),
-                hintText: 'User Name',
-                labelText: 'User Name',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: SimpleCrudStream.txtLastNameEditingController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                      25,
-                    ),
+                  const Text('Female'),
+                  Radio(
+                    value: SimpleCrudStream.feMale,
+                    groupValue: SimpleCrudStream.gender,
+                    onChanged: (value) {
+                      SimpleCrudStream.genderControllerValue =
+                          SimpleCrudStream.gender = value!;
+                    },
                   ),
-                ),
-                hintText: 'Last Name',
-                labelText: 'Last Name',
+                ],
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 15,
-              top: 10,
-              bottom: 5,
-            ),
-            child: Text(
-              "Select Gender",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          StreamBuilder(
-            stream: SimpleCrudStream.genderStreamController,
-            builder: (context, snapshot) => Column(
+            const SizedBox(height: 5),
+            Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Row(
-                    children: [
-                      Radio(
-                        value: SimpleCrudStream.male,
-                        groupValue: SimpleCrudStream.gender,
-                        onChanged: (value) {
-                          SimpleCrudStream.gender =
-                              SimpleCrudStream.genderControllerValue = value!;
-                        },
-                      ),
-                      const Text(
-                        'Male',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
+                const Text('Hobby :   '),
+                StreamBuilder(
+                  stream: SimpleCrudStream.hobbyCricketStreamController,
+                  builder: (context, snapshot) => Checkbox(
+                    value: SimpleCrudStream.isCricket,
+                    onChanged: (value) {
+                      SimpleCrudStream.hobbyCricketControllerValue =
+                          SimpleCrudStream.isCricket = value!;
+                    },
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Row(
-                    children: [
-                      Radio(
-                        value: SimpleCrudStream.feMale,
-                        groupValue: SimpleCrudStream.gender,
-                        onChanged: (value) {
-                          SimpleCrudStream.genderControllerValue =
-                              SimpleCrudStream.gender = value!;
-                        },
-                      ),
-                      const Text(
-                        'Female',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
+                const Text('Cricket'),
+                StreamBuilder(
+                  stream: SimpleCrudStream.hobbyFootballStreamController,
+                  builder: (context, snapshot) => Checkbox(
+                    value: SimpleCrudStream.isFootball,
+                    onChanged: (value) {
+                      SimpleCrudStream.hobbyFootballControllerValue =
+                          SimpleCrudStream.isFootball = value!;
+                    },
                   ),
                 ),
+                const Text('Football'),
+                StreamBuilder(
+                  stream: SimpleCrudStream.hobbySingingStreamController,
+                  builder: (context, snapshot) => Checkbox(
+                    value: SimpleCrudStream.isSinging,
+                    onChanged: (value) {
+                      SimpleCrudStream.hobbySingingControllerValue =
+                          SimpleCrudStream.isSinging = value!;
+                    },
+                  ),
+                ),
+                const Text('Singing'),
               ],
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 15,
-              top: 10,
-              bottom: 5,
-            ),
-            child: Text(
-              "Select Hobbies",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  children: [
-                    StreamBuilder(
-                      stream: SimpleCrudStream.hobbyCricketStreamController,
-                      builder: (context, snapshot) => Checkbox(
-                        value: SimpleCrudStream.isCricket,
-                        onChanged: (value) {
-                          SimpleCrudStream.hobbyCricketControllerValue =
-                              SimpleCrudStream.isCricket = value!;
-                        },
-                      ),
-                    ),
-                    const Text(
-                      'Cricket',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  children: [
-                    StreamBuilder(
-                      stream: SimpleCrudStream.hobbyFootballStreamController,
-                      builder: (context, snapshot) => Checkbox(
-                        value: SimpleCrudStream.isFootball,
-                        onChanged: (value) {
-                          SimpleCrudStream.hobbyFootballControllerValue =
-                              SimpleCrudStream.isFootball = value!;
-                        },
-                      ),
-                    ),
-                    const Text(
-                      'Football',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  children: [
-                    StreamBuilder(
-                      stream: SimpleCrudStream.hobbySingingStreamController,
-                      builder: (context, snapshot) => Checkbox(
-                        value: SimpleCrudStream.isSinging,
-                        onChanged: (value) {
-                          SimpleCrudStream.hobbySingingControllerValue =
-                              SimpleCrudStream.isSinging = value!;
-                        },
-                      ),
-                    ),
-                    const Text(
-                      'Singing',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Center(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  100,
-                ),
-                color: Colors.black,
-              ),
-              child: MaterialButton(
-                elevation: 15,
-                animationDuration: const Duration(
-                  seconds: 2,
-                ),
+            const SizedBox(height: 5),
+            // StreamBuilder(
+            //   stream: SimpleCrudStream.ageStreamController,
+            //   builder: (context, snapshot) => Slider(
+            //     value: SimpleCrudStream.selectedAge,
+            //     onChanged: (value) {
+            //       SimpleCrudStream.ageControllerValue =
+            //           SimpleCrudStream.selectedAge = value;
+            //     },
+            //     min: 0,
+            //     max: 100,
+            //   ),
+            // ),
+            const SizedBox(height: 5),
+            StreamBuilder(
+              stream: SimpleCrudStream.submitStreamController,
+              builder: (context, snapshot) => ElevatedButton(
                 onPressed: () {
-                  SimpleCrudStream.submitControllerValue = true;
+                  SimpleCrudStream.submitControllerValue =
+                      SimpleCrudStream.submited = true;
+
                   SimpleCrudStream.addUserData();
+                  SimpleCrudStream.userDataControllerValue =
+                      SimpleCrudStream.userData;
+                  SimpleCrudStream.clearData();
+                  SimpleCrudStream.submitControllerValue =
+                      SimpleCrudStream.submited = false;
                 },
-                child: const Text(
-                  'Submit',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
+                child: const Text('Submit'),
               ),
             ),
-          ),
-          StreamBuilder(
-              builder: (context, snapshot) => SimpleCrudStream
-                      .userData.isNotEmpty
-                  ? Expanded(
-                      child: ListView.builder(
-                        itemCount: SimpleCrudStream.userData.length,
-                        itemBuilder: (context, index) => Container(
-                          height: 200,
-                          width: double.infinity,
-                          color: Colors.tealAccent,
-                          child: Column(
-                            children: [
-                              StreamBuilder(
-                                stream: SimpleCrudStream.nameStreamController,
-                                builder: (context, snapshot) => Text(
-                                  snapshot.data.toString(),
+            const SizedBox(height: 5),
+            StreamBuilder(
+              stream: SimpleCrudStream.userDataStreamController,
+              builder: (context, snapshot) =>
+                  SimpleCrudStream.userData.isNotEmpty
+                      ? Expanded(
+                          child: ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) => GestureDetector(
+                              onTap: () {
+                                SimpleCrudStream.selecedIndex = index;
+                                SimpleCrudStream.onTapAdd();
+                                SimpleCrudStream.onUpdatTapAdd();
+                              },
+                              child: Dismissible(
+                                key: UniqueKey(),
+                                onDismissed: (direction) {
+                                  SimpleCrudStream.userData.removeAt(index);
+                                },
+                                child: Container(
+                                  height: 100,
+                                  width: double.infinity,
+                                  color: Colors.amber,
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                          'Name : ${snapshot.data![index]['name']}'),
+                                      Text(
+                                          'MiddleName : ${snapshot.data![index]['middleName']}'),
+                                      Text(
+                                          'LastName : ${snapshot.data![index]['lastName']}'),
+                                      Text(
+                                          'Gender : ${snapshot.data![index]['gender']}'),
+                                      Text(
+                                          'Hobby : ${snapshot.data![index]['hobby']}'),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
-                    )
-                  : const Center(child: Text('There is not data'))),
-        ],
+                        )
+                      : const Text('There is no data'),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/my_practice/simple_crud_app_demo/simple_crud_stream.dart
+++ lib/my_practice/simple_crud_app_demo/simple_crud_stream.dart
@@ -5,23 +5,27 @@ import 'package:flutter/material.dart';
 class SimpleCrudStream {
   static String gender = 'gender', male = 'male', feMale = 'feMale';
   static bool isCricket = false, isFootball = false, isSinging = false;
+  static double selectedAge = 0;
+  static int selecedIndex = 0;
   static bool submited = false;
   static List<String> selectHobbyList = [];
   static List<Map<String, dynamic>> userData = [];
   static TextEditingController txtNameEditingController =
       TextEditingController();
+  static TextEditingController txtMiddleNameEditingController =
+      TextEditingController();
   static TextEditingController txtLastNameEditingController =
       TextEditingController();
 
   static final StreamController<String> _streamController =
-      StreamController<String>();
+      StreamController<String>.broadcast();
   static Stream<String> get genderStreamController => _streamController.stream;
   static Sink<String> get genderSinkController => _streamController.sink;
   static set genderControllerValue(String data) =>
       genderSinkController.add(data);
 
   static final StreamController<bool> _streamCricketController =
-      StreamController<bool>();
+      StreamController<bool>.broadcast();
   static Stream<bool> get hobbyCricketStreamController =>
       _streamCricketController.stream;
   static Sink<bool> get hobbyCricketSinkController =>
@@ -30,7 +34,7 @@ class SimpleCrudStream {
       hobbyCricketSinkController.add(data);
 
   static final StreamController<bool> _streamFootballController =
-      StreamController<bool>();
+      StreamController<bool>.broadcast();
   static Stream<bool> get hobbyFootballStreamController =>
       _streamFootballController.stream;
   static Sink<bool> get hobbyFootballSinkController =>
@@ -39,7 +43,7 @@ class SimpleCrudStream {
       hobbyFootballSinkController.add(data);
 
   static final StreamController<bool> _streamSingingController =
-      StreamController<bool>();
+      StreamController<bool>.broadcast();
   static Stream<bool> get hobbySingingStreamController =>
       _streamSingingController.stream;
   static Sink<bool> get hobbySingingSinkController =>
@@ -47,12 +51,12 @@ class SimpleCrudStream {
   static set hobbySingingControllerValue(bool data) =>
       hobbySingingSinkController.add(data);
 
-  static final StreamController<bool> _streamSubmitController =
-      StreamController<bool>();
-  static Stream<bool> get submitStreamController =>
-      _streamSubmitController.stream;
-  static Sink<bool> get submitSinkController => _streamSubmitController.sink;
-  static set submitControllerValue(bool data) => submitSinkController.add(data);
+  static final StreamController<double> _streamSelectAgeController =
+      StreamController<double>.broadcast();
+  static Stream<double> get ageStreamController =>
+      _streamSelectAgeController.stream;
+  static Sink<double> get ageSinkController => _streamSelectAgeController.sink;
+  static set ageControllerValue(double data) => ageSinkController.add(data);
 
   static final StreamController<List<String>> _streamSelectHobbyController =
       StreamController<List<String>>();
@@ -64,18 +68,48 @@ class SimpleCrudStream {
       selectHobbySinkController.add(data);
 
   static final StreamController<String> _streamNameController =
-      StreamController<String>();
+      StreamController<String>.broadcast();
   static Stream<String> get nameStreamController =>
       _streamNameController.stream;
   static Sink<String> get nameSinkController => _streamNameController.sink;
   static set nameControllerValue(String data) => nameSinkController.add(data);
-  static Stream<String> get lastNameStreamController =>
-      _streamNameController.stream;
-  static Sink<String> get lastNameSinkController => _streamNameController.sink;
-  static set lastNameControllerValue(String data) =>
-      nameSinkController.add(data);
 
-  static addUserData() async* {
+  static final StreamController<String> _streamMiddleNameController =
+      StreamController<String>.broadcast();
+  static Stream<String> get middleNameStreamController =>
+      _streamMiddleNameController.stream;
+  static Sink<String> get middleNameSinkController =>
+      _streamMiddleNameController.sink;
+  static set middleNameControllerValue(String data) =>
+      middleNameSinkController.add(data);
+
+  static final StreamController<String> _streamLastNameController =
+      StreamController<String>.broadcast();
+  static Stream<String> get lastNameStreamController =>
+      _streamLastNameController.stream;
+  static Sink<String> get lastNameSinkController =>
+      _streamLastNameController.sink;
+  static set lastNameControllerValue(String data) =>
+      lastNameSinkController.add(data);
+
+  static final StreamController<bool> _streamSubmitController =
+      StreamController<bool>.broadcast();
+  static Stream<bool> get submitStreamController =>
+      _streamSubmitController.stream;
+  static Sink<bool> get submitSinkController => _streamSubmitController.sink;
+  static set submitControllerValue(bool data) => submitSinkController.add(data);
+
+  static final StreamController<List<Map<String, dynamic>>>
+      _streamUseDataController =
+      StreamController<List<Map<String, dynamic>>>.broadcast();
+  static Stream<List<Map<String, dynamic>>> get userDataStreamController =>
+      _streamUseDataController.stream;
+  static Sink<List<Map<String, dynamic>>> get userDataSinkController =>
+      _streamUseDataController.sink;
+  static set userDataControllerValue(List<Map<String, dynamic>> data) =>
+      userDataSinkController.add(data);
+
+  static addUserData() {
     selectHobbyList.clear();
     if (isCricket == true) {
       selectHobbyList.add('Cricket');
@@ -88,9 +122,56 @@ class SimpleCrudStream {
     }
     userData.add({
       'name': txtNameEditingController.text,
+      'middleName': txtMiddleNameEditingController.text,
       'lastName': txtLastNameEditingController.text,
       'gender': gender,
-      'hobby': List.from(selectHobbyList.map((e) => (e))),
+      'hobby': List.from(selectHobbyList.map((e) => e)),
     });
+  }
+
+  static onTapAdd() {
+    txtNameEditingController.text = userData[selecedIndex]['name'];
+    txtMiddleNameEditingController.text = userData[selecedIndex]['middleName'];
+    txtLastNameEditingController.text = userData[selecedIndex]['lastName'];
+    gender = userData[selecedIndex]['gender'];
+    selectHobbyList = userData[selecedIndex]['hobby'].map((e) => e);
+    if (userData[selecedIndex]['hobby'].contains('Cricket')) {
+      isCricket = true;
+    }
+    if (userData[selecedIndex]['hobby'].contains('Football')) {
+      isFootball = true;
+    }
+    if (userData[selecedIndex]['hobby'].contains('Singing')) {
+      isSinging = true;
+    }
+    selectHobbyList.clear();
+  }
+
+  static onUpdatTapAdd() {
+    if (isCricket == true) {
+      selectHobbyList.add('Cricket');
+    }
+    if (isFootball == true) {
+      selectHobbyList.add('Football');
+    }
+    if (isSinging == true) {
+      selectHobbyList.add('Singing');
+    }
+    userData[selecedIndex]['name'] = txtNameEditingController.text;
+    userData[selecedIndex]['middleName'] = txtMiddleNameEditingController.text;
+    userData[selecedIndex]['lastName'] = txtLastNameEditingController.text;
+    userData[selecedIndex]['gender'] = gender;
+    userData[selecedIndex]['hobby'] = List.from(selectHobbyList.map((e) => e));
+  }
+
+  static void clearData() {
+    txtNameEditingController.clear();
+    txtMiddleNameEditingController.clear();
+    txtLastNameEditingController.clear();
+    gender = 'gender';
+    selectHobbyList.clear();
+    isCricket = false;
+    isFootball = false;
+    isSinging = false;
   }
 }
